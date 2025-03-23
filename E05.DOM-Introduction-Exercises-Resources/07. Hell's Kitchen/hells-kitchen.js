460adb40b9196a6618f5301f85be9000bb1cbeae
trkolev <trkphv@gmail.com>
2025-03-23 15:53:16 +0200

    09

--- E05.DOM-Introduction-Exercises-Resources/07. Hell's Kitchen/hells-kitchen.js
+++ E05.DOM-Introduction-Exercises-Resources/07. Hell's Kitchen/hells-kitchen.js
@@ -1,26 +1,57 @@
 function solve() {
 
-    let text = document.getElementById('inputs').textContent;
-    let input = text.split(',');
+    let text = document.querySelector('#inputs textarea').value;
+    let clearedText = text.slice(2,-2);
 
-    let restaurants = {};
+    let input = clearedText.split('","');
 
-    for(let restaurant of input){
 
-        let currentRestaurant = new Map;
+
+    let restaurants = [];
+
+    for (let restaurant of input) {
+
+        let workers = new Map;
+        let currentRestaurant = {};
         let splitedRestaurant = restaurant.split(' - ');
         let restaurantName = splitedRestaurant[0];
         let pplInput = splitedRestaurant[1].split(', ');
+        let averageCelery = 0;
+        let bestCelery = 0;
 
-        for(let man of pplInput){
+        for (let man of pplInput) {
 
-            let[name, celery] = man.split(' ');
-            currentRestaurant.set(name, parseInt(celery));
-
+            let [name, celery] = man.split(' ');
+            workers.set(name, parseInt(celery));
+            averageCelery += parseInt(celery);
+            if (bestCelery < parseInt(celery)){
+                bestCelery = parseInt(celery);
+            }
         }
 
-        restaurants[restaurantName] = currentRestaurant;
+        currentRestaurant.name = restaurantName;
+        currentRestaurant.workers = workers;
+        currentRestaurant.average = averageCelery / pplInput.length;
+        currentRestaurant.highestCelery = bestCelery;
+
+        restaurants.push(currentRestaurant);
 
     }
 
+    restaurants.sort((a, b) => b.highestCelery - a.highestCelery);
+
+    let bestRestaurant = document.querySelector('#bestRestaurant p');
+    bestRestaurant.textContent = `Name: ${restaurants[0].name} Average Salary: ${restaurants[0].average.toFixed(2)} Best Salary: ${restaurants[0].highestCelery.toFixed(2)}`;
+
+    const sortedWorkers = new Map([...restaurants[0].workers.entries()].sort((a, b) => b[1] - a[1]));
+
+    let workers = document.querySelector('#workers p');
+    
+    let output = [];
+
+    sortedWorkers.forEach((salary, name) => {
+            output.push(`Name: ${name} With Salary: ${salary}`);
+    });
+
+    workers.textContent = output.join(' ');
 }
--- E05.DOM-Introduction-Exercises-Resources/09. Number Convertor/number-convertor.js
+++ E05.DOM-Introduction-Exercises-Resources/09. Number Convertor/number-convertor.js
@@ -1,3 +1,17 @@
 function solve() {
-    // TODO
+
+    let input = parseInt(document.getElementById('input').value);
+
+    let targetSystem = document.getElementById('selectMenuTo').value;
+
+    let result;
+
+    if ('binary' === targetSystem) {
+        result = input.toString(2);
+    } else if ('hexadecimal' === targetSystem){
+        result = input.toString(16).toUpperCase();
+    }
+
+    document.getElementById('result').value = result;
+
 }
