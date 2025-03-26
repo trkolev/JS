b6c0481e86819ec8c84f6559109f67cf478aa80f
trkolev <trkphv@gmail.com>
2025-03-26 19:21:13 +0200

    01

--- E06-JS-Front-End-DOM-Events-Exercise-Resources/01. Sections/sections.js
+++ E06-JS-Front-End-DOM-Events-Exercise-Resources/01. Sections/sections.js
@@ -1,5 +1,33 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+
+   let input = document.querySelector('#task-input input[type = text]').value;
+   let inputArr = input.split(', ');
+   
+   if (!input.trim()) {
+      return;
+   }
+   
+
+   let form = document.querySelector('#task-input');
+   form.addEventListener("submit", function (event){
+
+      event.preventDefault();
+
+      let content = document.getElementById('content');
+
+
+      for (let element of inputArr) {
+
+         let div = document.createElement("div");
+         let p = document.createElement("p");
+
+         p.textContent = element;
+
+         div.append(p);
+         content.append(div);
+
+      }
+   });
 }
