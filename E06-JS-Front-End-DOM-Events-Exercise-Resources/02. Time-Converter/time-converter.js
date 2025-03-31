d9073f3bc6f5e31e3b2149ed8903336fae4374fc
trkolev <trkphv@gmail.com>
2025-03-31 20:56:17 +0300

    Last

--- E06-JS-Front-End-DOM-Events-Exercise-Resources/02. Time-Converter/time-converter.js
+++ E06-JS-Front-End-DOM-Events-Exercise-Resources/02. Time-Converter/time-converter.js
@@ -1,51 +1,47 @@
 document.addEventListener('DOMContentLoaded', solve);
-
+ 
 function solve() {
-  
-    let daysOutput = document.getElementById('days-input');
-    let hoursOutput = document.getElementById('hours-input');
-    let minutesOutput = document.getElementById('minutes-input');
-    let secondsOutput = document.getElementById('seconds-input');
-
-
-    document.querySelector('#days input[type = submit]').addEventListener('click', function(event){
-        event.preventDefault();
-        hoursOutput.value = (Number(daysOutput.value) * 24).toFixed(2);
-        minutesOutput.value = (Number(daysOutput.value) * 1440).toFixed(2);
-        secondsOutput.value = (Number(daysOutput.value) * 86400).toFixed(2);
-        daysOutput.value = Number(daysOutput.value).toFixed(2);
-
-    });
-
-    document.querySelector('#hours input[type = submit]').addEventListener('click', function(event){
-        event.preventDefault();
-
-        daysOutput.value = (Number(hoursOutput.value) / 24).toFixed(2);
-        minutesOutput.value = (Number(hoursOutput.value) * 60).toFixed(2);
-        secondsOutput.value = (Number(hoursOutput.value) * 3600).toFixed(2);
-        hoursOutput.value = Number(hoursOutput.value).toFixed(2);
-
-    });
-
-    document.querySelector('#minutes input[type = submit]').addEventListener('click', function(event){
-        event.preventDefault();
-
-        daysOutput.value = Number(minutesOutput.value / 1440).toFixed(2);
-        hoursOutput.value = Number(minutesOutput.value / 60).toFixed(2);
-        secondsOutput.value = Number(minutesOutput.value * 60).toFixed(2);
-        minutesOutput.value = Number(minutesOutput.value).toFixed(2);
-
-    });
-
-
-    document.querySelector('#seconds input[type = submit]').addEventListener('click', function(event){
-        event.preventDefault();
-        
-        daysOutput.value = Number(secondsOutput.value / 86400).toFixed(2);
-        hoursOutput.value = Number(secondsOutput.value / 3600).toFixed(2);
-        minutesOutput.value = Number(secondsOutput.value / 60).toFixed(2);
-        secondsOutput.value = Number(secondsOutput.value).toFixed(2);
-
-    });
-
+    let inputDays= document.getElementById('days-input');
+    let inputHours= document.getElementById('hours-input');
+    let inputMinutes = document.getElementById('minutes-input');
+    let inputSeconds = document.getElementById('seconds-input');
+ 
+    let daysBtn = document.getElementById('daysBtn');
+    let hoursbtn = document.getElementById('hoursBtn');
+    let minutesBtn = document.getElementById('minutesBtn');
+    let secondsBtn = document.getElementById('secondsBtn');
+ 
+    daysBtn.addEventListener('click', convertFromDays);
+    hoursbtn.addEventListener('click', convertFromHours);
+    minutesBtn.addEventListener('click', convertFromMinutes);
+    secondsBtn.addEventListener('click', convertFromSeconds);
+ 
+    function convertFromDays(e) {
+        e.preventDefault();
+        inputHours.value = (Number(inputDays.value) * 24).toFixed(2);
+        inputMinutes.value = (Number(inputDays.value) * 1440).toFixed(2);
+        inputSeconds.value = (Number(inputDays.value) * 86400).toFixed(2);
+    }
+ 
+    function convertFromHours(e) {
+        e.preventDefault();
+        inputDays.value = (Number(inputHours.value) / 24).toFixed(2);
+        inputMinutes.value = (Number(inputHours.value) * 60).toFixed(2);
+        inputSeconds.value = (Number(inputHours.value) * 3600).toFixed(2);
+    }
+ 
+    function convertFromMinutes(e) {
+        e.preventDefault();
+        inputDays.value = (Number(inputMinutes.value) / 1440).toFixed(2);
+        inputHours.value = (Number(inputMinutes.value) / 60).toFixed(2);
+        inputSeconds.value = (Number(inputDays.value) * 60).toFixed(2);
+    }
+ 
+    function convertFromSeconds(e){
+        e.preventDefault();
+        inputDays.value = (Number(inputMinutes.value) / 86400).toFixed(2);
+        inputHours.value = (Number(inputSeconds.value) / 3600).toFixed(2);
+        inputMinutes.value = (Number(inputSeconds.value) / 60).toFixed(2);
+    }
+ 
 }
--- E06-JS-Front-End-DOM-Events-Exercise-Resources/08. Sudomu/sudomu.js
+++ E06-JS-Front-End-DOM-Events-Exercise-Resources/08. Sudomu/sudomu.js
@@ -1,20 +1,54 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    
-    document.querySelector('.buttons input[type = submit]').addEventListener('click', function (e){
 
-            let firstRow = document.querySelectorAll('table tr td:nth-child(1)');
-            let secondRow = document.querySelectorAll('table tr td:nth-child(2)');
-            let thirdRow = document.querySelectorAll('table tr td:nth-child(3)');
+    document.getElementById('solutionCheck').addEventListener('submit', function (e) {
+        e.preventDefault();
 
-            console.log(firstRow, secondRow, thirdRow);
+        let rows = document.querySelectorAll('#solutionCheck table tbody tr');
+        let data = [];
 
-    });
+        for (let row of rows) {
 
-    document.querySelector('.buttons input[type = reset]').addEventListener('click', function (e){
+            let currentRow = [];
 
-        
+            row.querySelectorAll('input').forEach(e => {
+                currentRow.push(Number(e.value));
+            });
+
+            data.push(currentRow);
+
+        }
+
+        let rowsAreFine = true;
+        let columnsAreFine = true;
+
+        for (let i = 0; i < 3; i++) {
+
+            if (data[i][0] === data[i][1] || data[i][0] === data[i][2] || data[i][1] === data[i][2]) {
+                rowsAreFine = false;
+            }
+
+        }
+
+        for (let j = 0; j < 3; j++) {
+
+            if (data[0][j] === data[1][j] || data[0][j] === data[2][j] || data[1][j] === data[2][j]) {
+                columnsAreFine = false;
+            }
+
+        }
+
+        let result = document.getElementById('check');
+
+
+        if (rowsAreFine && columnsAreFine) {
+            result.textContent = 'Success!';
+            document.querySelector('#solutionCheck table').style.border = '2px solid green';
+        } else {
+            result.textContent = 'Keep trying ...';
+            document.querySelector('#solutionCheck table').style.border = '2px solid red';
+        }
 
     });
 
